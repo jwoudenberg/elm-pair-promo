f1e47f0d0e99259d78747a940f0257dd4365436f
Jasper Woudenberg <mail@jasperwoudenberg.com>
2019-07-11 20:51:23 +0100

    Recording script runs in fixed-sized terminal
    I'd like the terminal recordings I make to not depend on the size of the
    terminal window. Also, it should be a bit smaller than what a fullscreen
    terminal on this machine allows.

--- studio/record.sh
+++ studio/record.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
+# Store original terminal dimensions.
+COLS="$(tput cols)"
+ROWS="$(tput lines)"
+
+# Set fixed terminal size for recording.
+stty columns 80
+stty rows 30
+
+# Record.
 asciinema rec rec.cast --command="vim -u plugin.vim ../src/Main.elm"
+
+# Restore original terminal dimensions.
+stty columns "$COLS"
+stty rows "$ROWS"
